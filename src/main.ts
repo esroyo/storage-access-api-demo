7f98defabbf39c3762bcbaca2ad7d60848181b8f
Carles Escrig Royo <esroyo@gmail.com>
2023-10-05 00:59:49 +0200

    refactor: simplify reponse

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ Deno.serve(async (req: Request) => {
     status: 200,
     headers: new Headers([
       ['Content-Type', 'text/html'],
+      ['Cross-Origin-Opener-Policy', 'unsafe-none'],
     ]),
   };
   const isIframed = req.headers.get('Sec-Fetch-Dest') === 'iframe';
@@ -27,15 +28,6 @@ Deno.serve(async (req: Request) => {
 
   const jsCode = await Deno.readTextFile('./src/snippet.js');
 
-  if (isIframed) {
-        if (cookieJar.has('user')) {
-            return new Response(`Hello, ${cookieJar.get('user')}! <form><input type="hidden" name="logout" value="true"/><button>Logout</button></form>`, res);
-        } else {
-            return new Response(`<button id="button">Check credentials</button><p>If nothing happens, you need to login</p><script>${jsCode}</script>`, res);
-        }
-
-    }
-
   const inputUser = url.searchParams.get('user');
   if (inputUser === '') {
     cookieJar.delete('user', cookieOptions);
@@ -49,9 +41,32 @@ Deno.serve(async (req: Request) => {
     res.headers.set('Location', url.origin);
     return new Response('', res);
   }
-  if (cookieJar.has('user') || inputUser) {
-      return new Response(`Hello, ${cookieJar.get('user') || inputUser}! <form><input type="hidden" name="logout" value="true"/><button>Logout</button></form>`, res);
-  } else {
-      return new Response(`<form><input name="user"></intput> <button id="button">Login</button></form><script>${jsCode}</script>`, res);
-  }
+
+const linkAttrs = isIframed ? ' rel="opener" target="_blank"' : '';
+const logoutNotice = isIframed ? '' : '<p><marquee direction="down" width="600px" height="100px">⚠ This tab will auto-close in some seconds ⚠</marquee></p>';
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login demo</title>
+  <style>.hide { display: none; }</style>
+</head>
+<body>
+<section id="check" class="hide">
+  I don't know who you are. Let me check by clicking here:
+  <button id="checkButton">Check</button>
+</section>
+<section id="hello" class="hide">
+  Hello, <span id="userName"></span>! <form><input type="hidden" name="logout" value="true"/><button>Logout</button></form>
+  ${logoutNotice}
+</section>
+<section id="login" class="hide">
+  <form${linkAttrs}><input id="user" name="user" value=""></intput><button>Login</button></form>
+</section>
+<script>${jsCode}</script>
+</html>
+`;
+    return new Response(html, res);
 });
--- src/snippet.js
+++ src/snippet.js
@@ -1,37 +1,65 @@
-function doThingsWithCookies() {
+async function doThingsWithCookies() {
+    check.classList.add('hide');
+    console.log('document.cookie:', document.cookie);
     if (document.cookie) {
         const [key, value] = document.cookie.split('=');
-        button.parentNode.innerHTML = `Hello, ${value}! <form><input type="hidden" name="logout" value="true"/><button>Logout</button></form>`;
+        userName.textContent = value;
+        hello.classList.remove('hide');
+        if (window.opener) {
+          window.opener.postMessage({ action: 'reload' });
+          setTimeout(() => { window.close(); }, 7000);
+        }
+    } else {
+        login.classList.remove('hide');
     }
 }
 
 async function handleCookieAccess() {
-    if (!document.hasStorageAccess) {
-        // This browser doesn't support the Storage Access API
-        // so let's just hope we have access!
+    const hasAccess = await (document.hasStorageAccess?.() ?? true);
+    if (hasAccess) {
+        console.log('has access');
+        // We have access to unpartitioned cookies, so let's go
         doThingsWithCookies();
     } else {
-        const hasAccess = await document.hasStorageAccess();
-        if (hasAccess) {
-            console.log('has access');
-            // We have access to unpartitioned cookies, so let's go
-            doThingsWithCookies();
-        } else {
-            console.log('does not have access');
-            // Need to call requestStorageAccess() after a user interaction
-            button.addEventListener("click", () => {
-                try {
-                    document.requestStorageAccess().then(() => {
-                        doThingsWithCookies();
-                    });
-                } catch (err) {
-                    // If there is an error obtaining storage access.
-                    console.error(`Error obtaining storage access: ${err}.
-                            Please sign in.`);
-                }
+        console.log('does not have access');
+
+        let permission;
+        try {
+            permission = await navigator.permissions.query({
+                name: "storage-access",
             });
+        } catch (error) {}
+
+        console.log('permission query result', permission);
+        if (!permission || permission.state === 'denied' || permission.state === 'prompt') {
+            check.classList.remove('hide');
+            console.log('Need to call requestStorageAccess() after a user interaction');
+            checkButton.addEventListener("click", () => {
+                document.requestStorageAccess().then(doThingsWithCookies, doThingsWithCookies);
+            });
+        } else {
+            console.log('Should be possible to call requestStorageAccess() without user interaction...');
+            console.log('According to docs (point 5): https://developer.mozilla.org/en-US/docs/Web/API/Storage_Access_API/Using#checking_and_requesting_storage_access');
+            try {
+                await document.requestStorageAccess();
+            } catch (err) {
+                console.log('However it fails:', err);
+                check.classList.remove('hide');
+                console.log('Need to call requestStorageAccess() after a user interaction');
+                checkButton.addEventListener("click", () => {
+                    document.requestStorageAccess().then(doThingsWithCookies, doThingsWithCookies);
+                });
+            }
         }
     }
 }
 
+async function setupEventListeners() {
+  window.addEventListener('message', (ev) => {
+    console.log('received postMessage', ev?.data);
+    if (ev?.data?.action === 'reload') location.reload();
+  });
+}
+
+setupEventListeners();
 handleCookieAccess();
